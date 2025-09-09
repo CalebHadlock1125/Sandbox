431d0da9a8f62adf49e18660dc3bf9b01309ff6c
alecsearle <searle.alec@gmail.com>
2025-09-08 19:15:24 -0600

    phone syling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>Mira</div>
-    </>
+    <div className="app-container">
+      <h1>Mira</h1>
+    </div>
   );
 }
 
