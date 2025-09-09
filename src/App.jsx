b275b76029a59218c68b3e40c50a251a493065ad
alecsearle <searle.alec@gmail.com>
2025-09-08 19:30:22 -0600

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="app-container">
       <h1>Mira</h1>
